316c104eaabd85dde2b85a17015806928e40ef76
Daniela Pinedo <daniela.pinedo@pragma.com.co>
2019-12-16 15:16:12 -0500

    Prueba completa 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+
+//Componente 1: 
+//¿Qué debo pasar? 
+//¿Qué resultado obtengo? 
+
+//Jest retorna objeto que permite leer que ha ocurrido a la función, con que se llamo y cuantas veces se llamo
+export function Todo({ todo, index, completeTodo, removeTodo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>X</button>
+      </div>
+    </div>
+  );
+}
+
+export function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export const useTodos = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: "Todo 1",
+      isCompleted: false
+    },
+    {
+      text: "Todo 2",
+      isCompleted: false
+    },
+    {
+      text: "Todo 3",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [{ text }, ...todos];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return {todos,addTodo, completeTodo, removeTodo}
+} 
+
+function App() {
+
+ const {todos,addTodo, completeTodo, removeTodo} = useTodos();
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,154 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App,{Todo, TodoForm, useTodos} from './App';
+import {shallow, mount, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter: new Adapter()}); // ejecuta test sobre los componentes
+
+describe("App", () => {
+  describe("Todo", () =>{
+    it('ejecuta completeTodo cuando doy click en Complete', () => {
+        const completeTodo = jest.fn(); // crea Mock
+        const removeTodo = jest.fn();
+        const index = 5;
+        const todo ={
+          isComplete: true,
+          text:'lala'
+        };
+        const wrapper = shallow(<Todo
+          completeTodo = {completeTodo}
+          removeTodo   = {removeTodo}
+          index = {index}
+          todo = {todo}
+        />)
+        
+        wrapper
+          .find('button')
+          .at(0)
+          .simulate('click');
+        
+        expect(completeTodo.mock.calls).toEqual([[5]]);
+        expect(removeTodo.mock.calls).toEqual([]);
+        //completeTodo.mock.calls === [];  Número de veces que se llamo la función.
+    });
+
+    it('ejecuta removeTodo cuando doy click en X', () => {
+      const completeTodo = jest.fn(); // crea Mock
+      const removeTodo = jest.fn();
+      const index = 5;
+      const todo ={
+        isComplete: true,
+        text:'lala'
+      };
+      const wrapper = shallow(<Todo
+        completeTodo = {completeTodo}
+        removeTodo   = {removeTodo}
+        index = {index}
+        todo = {todo}
+      />)
+      
+      wrapper
+        .find('button')
+        .at(1)
+        .simulate('click');
+      
+      expect(removeTodo.mock.calls).toEqual([[5]]);
+      expect(completeTodo.mock.calls).toEqual([]);
+    });
+  });
+
+  describe('TodoForm',() => {
+    it('Llamar a addTodo cundo el formulario tiene un valor', () => {
+       const addTodo = jest.fn();
+       const prevent = jest.fn();
+       const wrapper = shallow (<TodoForm addTodo = {addTodo}/>);
+
+       wrapper
+        .find('input')
+        .simulate('change', {target: {value:"mi nuevo todo!"}});
+       wrapper
+        .find('form')
+        .simulate('submit', {preventDefault: prevent });
+        expect(addTodo.mock.calls).toEqual([['mi nuevo todo!']]);
+        expect(prevent.mock.calls).toEqual([[]]);
+    });
+  });
+
+  // requiere orden de ejecución y componente de ejecución
+  describe("custom hook: useTodos", () => {
+    it('addTodo', () => {
+        const Test = (props) => { // componente de prueba
+          const hook = props.hook();
+          return <div {...hook}></div>;
+        }
+
+        const wrapper = shallow (<Test hook={useTodos}/>)
+        let props = wrapper.find('div').props();
+        props.addTodo('Texto de prueba');
+        props = wrapper.find('div').props(); // obtener valores de prueba
+        expect(props.todos[0]).toEqual({text: 'Texto de prueba'});
+    });
+
+    it("completeTodo", () => {
+      const Test = props => { // componente de prueba
+        const hook = props.hook();
+        return <div {...hook}></div>;
+      }
+
+      const wrapper = shallow (<Test hook={useTodos}/>)
+      let props = wrapper.find('div').props();
+      props.completeTodo(0);
+      props = wrapper.find('div').props(); // obtener valores de prueba
+      expect(props.todos[0]).toEqual({text: "Todo 1" , isCompleted: true});
+    });
+
+    it("removeTodo", () => {
+      const Test = props => { // componente de prueba
+        const hook = props.hook();
+        return <div {...hook}></div>;
+      }
+
+      const wrapper = shallow (<Test hook={useTodos}/>)
+      let props = wrapper.find('div').props();
+      props.removeTodo(0);
+      props = wrapper.find('div').props(); // obtener valores de prueba
+      expect(props.todos).toEqual([
+        {
+          text: "Todo 2",
+          isCompleted: false
+        },
+        {
+          text: "Todo 3",
+          isCompleted: false
+        }
+      ]);
+    });
+
+    it ('App', () => {
+      const wrapper = mount(<App/>)
+      const prevent = jest.fn();
+
+      wrapper
+      .find('input')
+      .simulate('change', {target: {value:"mi todo!"}});
+
+      wrapper
+        .find('form')
+        .simulate('submit', {preventDefault: prevent });
+      
+      const respuesta = wrapper
+      .find('.todo')
+      .at(0)
+      .text()
+      .includes('mi todo!')
+
+      console.log(respuesta);
+
+      expect(respuesta).toEqual(true);
+      expect(prevent.mock.calls).toEqual([[]]);
+    });
+
+  });
 });
+
